81e518bf40767cee822230725a584294a18cac26
Asit Ghosh <asitg2003@gmail.com>
2023-12-24 16:19:13 +0530

    Create First app

---
+++ hello/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("This is HomePage")
+    return render(request,'index.html')
+
+def about(request):
+    return HttpResponse("This is about")
+
+def services(request):
+    return HttpResponse("This is services")
+
+def contact(request):
+    return HttpResponse("This is contact")
