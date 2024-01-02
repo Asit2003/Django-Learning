9475c36b47a1c20563bf331597d3d46926fd3486
Asit Ghosh <asitg2003@gmail.com>
2024-01-02 19:31:20 +0530

    Home Page Completed

--- hello/home/views.py
+++ hello/home/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
+    context = {
+        "variable":"It's Me ASIT"
+    }
     # return HttpResponse("This is HomePage")
-    return render(request,'index.html')
+    return render(request,'index.html', context)
 
 def about(request):
-    return HttpResponse("This is about")
+    return render(request,'about.html')
 
 def services(request):
-    return HttpResponse("This is services")
+    return render(request,'services.html')
 
 def contact(request):
-    return HttpResponse("This is contact")
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email,desc=desc,date=datetime.today())
+        contact.save()
+    return render(request,'contact.html')
